391a3b9c95e2d3531163cd216226eb3b33ba7720
samin sadat <saminsadat.cse@gmail.com>
2025-03-09 05:15:20 +0600

    added about-section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const About = () => {
   return (
     <div
-      className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:h-32 w-full overflow-hidden'
+      className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
       id='About'
     >
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>
-        About{' '}
+        About
         <span className='underline underline-offset-4 decoration-1 under font-light'>
           Our Brand
         </span>
@@ -15,6 +16,44 @@ const About = () => {
       <p className='text-gray-500 max-w-80 text-center mb-8'>
         Passionate About Properties, Dedicated to Your Vision
       </p>
+      <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+        <img
+          src={assets.brand_img}
+          alt=''
+          className='w-full sm:w-1/2 max-w-lg'
+        />
+        <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+          <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+            <div>
+              <p className='text-4xl font-medium text-gray-800'>10+</p>
+              <p className=''>Years of Experience</p>
+            </div>
+            <div>
+              <p className='text-4xl font-medium text-gray-800'>12+</p>
+              <p className=''>Project Completed</p>
+            </div>
+            <div>
+              <p className='text-4xl font-medium text-gray-800'>20+</p>
+              <p className=''>Mn. Sq. Ft. Delivered</p>
+            </div>
+            <div>
+              <p className='text-4xl font-medium text-gray-800'>25+</p>
+              <p className=''>Ongoing Projects</p>
+            </div>
+          </div>
+          <p className='my-10 max-w-lg'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum est,
+            iste culpa quasi tempora error commodi nobis itaque, enim excepturi
+            maxime incidunt autem debitis omnis in quod impedit rerum, libero
+            magnam. Perferendis veritatis nam voluptatibus? Facilis dicta autem
+            voluptatem sapiente excepturi perferendis tempore, neque magni qui,
+            et alias rerum facere.
+          </p>
+          <button className='bg-blue-600 text-white px-8 py-2 rounded'>
+            Learn more
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
